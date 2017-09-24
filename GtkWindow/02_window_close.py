52ad15eeb2872d7dd5d393d7aa7211be1865b632
Julita Inca <jinca@pucp.pe>
2017-09-24 17:50:40 -0500

    Removing a directory

--- GtkWindow/02_window_close.py
+++
@@ -1,6 +0,0 @@
-from gi.repository import Gtk
-
-window = Gtk.Window()
-window.connect("destroy",Gtk.main_quit)
-window.show()
-Gtk.main()
--- GtkWindow/04_window_size.py
+++
@@ -1,8 +0,0 @@
-from gi.repository import Gtk
-
-window = Gtk.Window()
-window.connect("destroy",Gtk.main_quit)
-window.set_default_size(400,600)
-window.show()
-
-Gtk.main()
