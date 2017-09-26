c77e8fd05d4a2dc02a98a34cc96751b82e4c845d
Julita Inca <jinca@pucp.pe>
2017-09-25 23:44:59 -0500

    Removing a file 07

--- GtkWindow/07_window_button.py
+++
@@ -1,9 +0,0 @@
-from gi.repository import Gtk
-
-window = Gtk.Window()
-button = Gtk.Button("Click it!")
-
-window.add(button)
-window.connect("destroy",Gtk.main_quit)
-window.show_all()
-Gtk.main()
