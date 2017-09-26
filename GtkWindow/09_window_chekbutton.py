92bf85998dd567c944f882bbf5de805882a7c8de
Julita Inca <jinca@pucp.pe>
2017-09-25 23:48:04 -0500

    Removing a file 09

--- GtkWindow/09_window_chekbutton.py
+++
@@ -1,9 +0,0 @@
-from gi.repository import Gtk
-
-window = Gtk.Window()
-check = Gtk.CheckButton("This is a check button")
-
-window.add(check)
-window.connect("destroy", Gtk.main_quit)
-window.show_all()
-Gtk.main()
