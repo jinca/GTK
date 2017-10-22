c4951e885f1c4fbcfa767893dd6e6ba8d86f9350
Julita Inca <jinca@pucp.pe>
2017-10-22 13:07:44 -0500

    Working with button that print a message on terminal

---
+++ StartingGTK_C/window04.c
@@ -0,0 +1,41 @@
+#include<gtk/gtk.h>
+
+static void button_clicked(GtkWidget* widget, gpointer data)
+
+{
+
+       g_print("Welcome to APISTRAT ");
+
+}
+
+int main(int argc, char* argv[])
+
+{
+
+      gtk_init(&argc,&argv);
+
+      GtkWidget *window, *label, *button;
+
+      button = gtk_button_new_with_label("Click me");
+
+      g_signal_connect(button,"clicked",G_CALLBACK(button_clicked), NULL);
+
+      window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+      
+      label = gtk_label_new("Welcome to APISTRAT");
+
+      gtk_window_set_default_size(GTK_WINDOW(window),300,300);
+
+      gtk_window_set_title(GTK_WINDOW(window),"Linux Foundation");
+
+      g_signal_connect(window,"delete_event",G_CALLBACK(gtk_main_quit), NULL);
+
+      gtk_container_add(GTK_CONTAINER(window),button);
+
+      gtk_widget_show_all(window);
+
+      gtk_main();
+
+      return 0;
+
+}
