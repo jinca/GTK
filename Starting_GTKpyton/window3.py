184dde1b8ef9f43189d6bf083eda844f54b687d5
Julita Inca <jinca@pucp.pe>
2017-10-30 23:42:36 -0500

    Adding a label to a window

--- Starting_GTKpyton/window3.py
+++ Starting_GTKpyton/window3.py
@@ -3,13 +3,11 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 window = Gtk.Window()
-label = Gtk.Label("Welcome to APISTRAT")
-
 window.set_title("Linux Foundation")
 window.set_default_size(400,600)
-
 window.connect("destroy",Gtk.main_quit)
+
+label = Gtk.Label("Welcome to APISTRAT")
 window.add(label)
 window.show_all()
-
 Gtk.main()
