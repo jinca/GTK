4831f1987999b1fd8d1b0e192a3e3e374664d9c7
Julita Inca <jinca@pucp.pe>
2017-10-30 23:37:12 -0500

    Adding a button into a window

--- Starting_GTKpyton/window4.py
+++ Starting_GTKpyton/window4.py
@@ -3,21 +3,20 @@ gi.require_version('Gtk','3.0')
 from gi.repository import Gtk
 
 def on_button_clicked(button):
-    print("Welcome to APISTRAT\n")
+    print("Welcome to APISTRAT")
 
 def on_destroy(window):
     Gtk.main_quit()
 
 window = Gtk.Window()
-button = Gtk.Button("Click me")
-
 window.set_title("Linux Foundation")
 window.set_default_size(400,600)
-button.connect("clicked",on_button_clicked)
-button.set_alignment(0,1)
-window.add(button)
 window.connect("destroy",on_destroy)
-window.show_all()
 
+button = Gtk.Button("Click me")
+button.connect("clicked",on_button_clicked)
+window.add(button)
+
+window.show_all()
 Gtk.main()
 
