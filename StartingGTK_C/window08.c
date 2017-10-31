c44a1795e93fd8217f44a580e65eb96d097ce231
Julita Inca <jinca@pucp.pe>
2017-10-30 23:26:32 -0500

    Working with an entry and a button to print a message

---
+++ StartingGTK_C/window08.c
@@ -0,0 +1,30 @@
+#include<gtk/gtk.h>
+static void button_clicked(GtkWidget *widget, gpointer data)
+{
+    g_print("%s\n",gtk_entry_get_text(GTK_ENTRY(data)));
+}
+
+int main(int argc, char* argv[])
+{
+        gtk_init(&argc,&argv);
+        GtkWidget *window,*entry,*button,*box;
+
+        window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+        gtk_window_set_title(GTK_WINDOW(window),"Linux Foundation");
+        gtk_window_set_default_size(GTK_WINDOW(window),300,200);
+	gtk_container_set_border_width(GTK_CONTAINER(window),50);
+        g_signal_connect(window,"delete_event",G_CALLBACK(gtk_main_quit),NULL);
+
+        entry = gtk_entry_new();
+        button =gtk_button_new_with_label("Click me");
+        g_signal_connect(button,"clicked",G_CALLBACK(button_clicked), entry);
+
+        box = gtk_box_new(GTK_ORIENTATION_VERTICAL,6);
+        gtk_box_pack_start(GTK_BOX(box),entry,TRUE,TRUE,0);
+        gtk_box_pack_start(GTK_BOX(box),button,TRUE,TRUE,0);
+        gtk_container_add(GTK_CONTAINER(window),box);
+
+        gtk_widget_show_all(window);
+        gtk_main();
+        return 0;
+}
--- Starting_GTKpyton/window8.py
+++ Starting_GTKpyton/window8.py
@@ -2,25 +2,23 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-def on_entry_activated(entry, label):
+def on_button_clicked(button,entry):
     text = entry.get_text()  
-    label.set_text(text)  
-    entry.set_text('')  
+    print(text)  
 
 window = Gtk.Window()
-window.set_title('Welcome to APISTRAT')
-window.set_border_width(10)
+window.set_title('Linux Foundation')
+window.set_border_width(50)
 window.connect('delete-event', Gtk.main_quit)
 
-box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-
-label = Gtk.Label('Enter a message and press <ENTER>...')
-
+button = Gtk.Button('Click me')
 entry = Gtk.Entry()
-entry.connect('activate', on_entry_activated, label)  
 
+button.connect('clicked', on_button_clicked, entry)  
+
+box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
 box.pack_start(entry, True, True, 0)
-box.pack_start(label, True, True, 0)
+box.pack_start(button, True, True, 0)
 
 window.add(box)
 window.show_all()
