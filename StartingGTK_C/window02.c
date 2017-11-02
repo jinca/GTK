da3a14af85629d97646f6a595d67e6e438a7c92f
Luis Menina <liberforce@freeside.fr>
2017-11-02 15:23:33 +0100

    No good reason to not use gtk_widget_show_all to show the app
    Even if you have a single widget, use show_all. Beginners tend to forget
    to change that line and afterwards don't understand why their widgets
    are not displayed after they added more.

--- StartingGTK_C/window02.c
+++ StartingGTK_C/window02.c
@@ -12,7 +12,7 @@ int main (int argc, char *argv[])
 
     g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
-    gtk_widget_show (window);
+    gtk_widget_show_all (window);
     gtk_main ();
     return 0;
 }
