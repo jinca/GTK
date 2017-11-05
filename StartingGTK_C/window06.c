cb344f2927b0ea2d293178b88bf5f368b47bd8f4
Julita Inca <jinca@pucp.pe>
2017-11-05 10:41:49 -0500

    Cleaning up the code with spaces and proper indentation

--- StartingGTK_C/window06.c
+++ StartingGTK_C/window06.c
@@ -1,28 +1,31 @@
-#include<gtk/gtk.h>
-static void button_clicked(GtkWidget* widget, gpointer data)
+#include <gtk/gtk.h>
+
+static void on_button_clicked (GtkWidget* widget, gpointer data)
 {
-      gtk_label_set_text(GTK_LABEL(data),"Welcome to APISTRAT");
+    gtk_label_set_text (GTK_LABEL (data), "Welcome to APISTRAT");
 }
-int main(int argc, char* argv[])
+
+int main (int argc, char* argv[])
 {
-      gtk_init(&argc,&argv);
-      GtkWidget *window, *label, *button, *box;
+    GtkWidget *window, *label, *button, *box;
 
-      window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-      gtk_window_set_default_size(GTK_WINDOW(window),400,600);
-      gtk_window_set_title(GTK_WINDOW(window),"Linux Foundation");
-      g_signal_connect(window,"delete_event",G_CALLBACK(gtk_main_quit), NULL);
-      
-      label = gtk_label_new("");
-      button = gtk_button_new_with_label("Click me");
-      
-      box = gtk_box_new(GTK_ORIENTATION_VERTICAL,6);
-      gtk_box_pack_start(GTK_BOX(box),button,TRUE,TRUE,0);
-      gtk_box_pack_start(GTK_BOX(box),label,TRUE,TRUE,0);
-      g_signal_connect(button,"clicked",G_CALLBACK(button_clicked), (gpointer)label);
+    gtk_init (&argc, &argv);
 
-      gtk_container_add(GTK_CONTAINER(window), box);
-      gtk_widget_show_all(window);
-      gtk_main();
-      return 0;
+    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size (GTK_WINDOW (window), 400, 600);
+    gtk_window_set_title (GTK_WINDOW (window), "Linux Foundation");
+
+    label = gtk_label_new (NULL);
+    button = gtk_button_new_with_label ("Click me");
+      
+    box = gtk_box_new (GTK_ORIENTATION_VERTICAL,6);
+    gtk_box_pack_start (GTK_BOX (box), button, TRUE, TRUE, 0);
+    gtk_box_pack_start (GTK_BOX (box), label, TRUE, TRUE, 0);
+    gtk_container_add (GTK_CONTAINER (window), box);
+
+    g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+    g_signal_connect (button, "clicked", G_CALLBACK (on_button_clicked), label);
+    gtk_widget_show_all (window);
+    gtk_main ();
+    return 0;
 }
