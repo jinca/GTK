c7b8bd8f1f2231ab2c0c6beec1bf559ecf5a2dde
Julita Inca <jinca@pucp.pe>
2017-11-05 00:59:33 -0500

    Cleaning up the code

--- StartingGTK_C/window03.c
+++ StartingGTK_C/window03.c
@@ -1,20 +1,21 @@
-#include<gtk/gtk.h>
+#include <gtk/gtk.h>
 
-int main(int argc, char* argv[])
+int main (int argc, char *argv[])
 {
-      gtk_init(&argc,&argv);
-      GtkWidget *window, *label;
+    GtkWidget *window, *label;
 
-      window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-      label = gtk_label_new("Welcome to APISTRAT");
+    gtk_init (&argc,&argv);
 
-      gtk_window_set_default_size(GTK_WINDOW(window),400,600);
-      gtk_window_set_title(GTK_WINDOW(window),"Linux Foundation");
-      g_signal_connect(window,"delete_event",G_CALLBACK(gtk_main_quit), NULL);
+    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size (GTK_WINDOW (window), 400, 600);
+    gtk_window_set_title (GTK_WINDOW (window), "Linux Foundation");
 
-      gtk_container_add(GTK_CONTAINER(window),label);
-      gtk_widget_show_all(window);
+    label = gtk_label_new ("Welcome to APISTRAT");
+    gtk_container_add (GTK_CONTAINER (window), label);
 
-      gtk_main();
-      return 0;
+    g_signal_connect (window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show_all (window);
+    gtk_main ();
+    return 0;
 }
