376731aa9bfe005d825cad822fe258f173912c4a
Julita Inca <jinca@pucp.pe>
2017-11-05 01:23:01 -0500

    Cleaning up the code with spaces and proper indentation

--- StartingGTK_C/window05.c
+++ StartingGTK_C/window05.c
@@ -1,27 +1,28 @@
-#include<gtk/gtk.h>
-static void button_clicked(GtkWidget* widget, gpointer data)
+#include <gtk/gtk.h>
+
+static void on_button_clicked (GtkWidget* widget, gpointer data)
 {
-       g_print("Welcome to APISTRAT\n");
+    g_print ("Welcome to APISTRAT\n");
 }
 
-int main(int argc, char* argv[])
+int main (int argc, char *argv[])
 {
-      gtk_init(&argc,&argv);
-      GtkWidget *window, *button;
+    GtkWidget *window, *button;
 
-      button = gtk_button_new_with_label("Click me");
-      gtk_widget_set_halign(button,GTK_ALIGN_CENTER);
-      gtk_widget_set_valign(button,GTK_ALIGN_CENTER);
-      g_signal_connect(button,"clicked",G_CALLBACK(button_clicked), NULL);
+    gtk_init (&argc,&argv);
 
-      window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-      gtk_window_set_default_size(GTK_WINDOW(window),300,300);
-      gtk_window_set_title(GTK_WINDOW(window),"Linux Foundation");
-      g_signal_connect(window,"delete_event",G_CALLBACK(gtk_main_quit), NULL);
+    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size (GTK_WINDOW (window), 300, 300);
+    gtk_window_set_title (GTK_WINDOW (window),"Linux Foundation");
 
-      gtk_container_add(GTK_CONTAINER(window),button);
+    button = gtk_button_new_with_label ("Click me");
+    gtk_widget_set_halign (button, GTK_ALIGN_CENTER);
+    gtk_widget_set_valign (button, GTK_ALIGN_CENTER);
+    gtk_container_add (GTK_CONTAINER (window), button);
 
-      gtk_widget_show_all(window);
-      gtk_main();
-      return 0;
+    g_signal_connect (window, "detroy", G_CALLBACK (gtk_main_quit), NULL);
+    g_signal_connect (button, "clicked", G_CALLBACK (on_button_clicked), NULL);
+    gtk_widget_show_all (window);
+    gtk_main ();
+    return 0;
 }
