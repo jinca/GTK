7ead81e71d776f6fb1a1f0c1ba9bf17518ddcb49
Julita Inca <jinca@pucp.pe>
2017-11-06 01:34:41 -0500

    Creating a window with a function to destroy a GTK window

--- StartingGTK_C/window01.c
+++ StartingGTK_C/window01.c
@@ -1,15 +1,15 @@
 #include <gtk/gtk.h>
 
-int main (int argc, char* argv[])
+int main (int argc, char *argv[])
 {
     GtkWidget *window;
 
     git_init (&argc, &argv);
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 
-    g_signal_connect (window,"destroy", G_CALLBACK (gtk_main_quit), NULL);
+    g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
-    gtk_widget_show_all(window);
-    gtk_main();
+    gtk_widget_show_all (window);
+    gtk_main ();
     return 0;
 }
