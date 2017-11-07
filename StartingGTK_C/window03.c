eac7cb2e3e3137fbc1c39ba74d649b4599cb1a29
Julita Inca <jinca@pucp.pe>
2017-11-06 21:58:28 -0500

    A label is going to be displayed into a GTK window

--- StartingGTK_C/window03.c
+++ StartingGTK_C/window03.c
@@ -4,7 +4,7 @@ int main (int argc, char *argv[])
 {
     GtkWidget *window, *label;
 
-    gtk_init (&argc,&argv);
+    gtk_init (&argc, &argv);
 
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
     gtk_window_set_default_size (GTK_WINDOW (window), 400, 600);
@@ -13,7 +13,7 @@ int main (int argc, char *argv[])
     label = gtk_label_new ("Welcome to APISTRAT");
     gtk_container_add (GTK_CONTAINER (window), label);
 
-    g_signal_connect (window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
     gtk_widget_show_all (window);
     gtk_main ();
