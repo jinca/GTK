e16486c48aead9be076222994ad25038b64cb838
Julita Inca <jinca@pucp.pe>
2017-11-10 23:47:23 -0500

    Creating a window with a function to destroy a GTK window

---
+++ StartingGTK_Python/window1.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+
+def main():
+    window = Gtk.Window()
+    window.connect("destroy", Gtk.main_quit)
+
+    window.show_all()
+    Gtk.main()
+
+
+if __name__ == "__main__":
+    main()
