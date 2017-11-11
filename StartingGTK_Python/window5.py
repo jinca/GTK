6d2f75d87a30cb3b135eb17a6f4eb242fb90b99b
Julita Inca <jinca@pucp.pe>
2017-11-11 00:00:46 -0500

    By clicking a button, align in the center, will print a message on terminal

---
+++ StartingGTK_Python/window5.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+
+import gi 
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+
+def on_button_clicked(button):
+    print("Welcome to APISTRAT")
+
+
+def on_destroy(window):
+    Gtk.main_quit()
+
+
+def main():
+    window = Gtk.Window()
+    window.set_title("Linux Foundation")
+    window.set_default_size(400, 600)
+    window.connect("destroy", on_destroy)
+
+    button = Gtk.Button("Click me")
+    button.set_halign(Gtk.Align.CENTER)
+    button.set_valign(Gtk.Align.CENTER)
+    button.connect("clicked",on_button_clicked)
+    window.add(button)
+
+    window.show_all()
+    Gtk.main()
+
+
+if __name__ == "__main__":
+    main()
